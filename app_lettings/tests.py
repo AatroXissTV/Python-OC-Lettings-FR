b1b3daa06db4dcf1b277f94bdd62b00008953f93
AatroXissTV <antoine.beaudesson@gmail.com>
2022-04-12 11:42:41 +0200

    v.0.1.2 added tests for lettings

--- app_lettings/tests.py
+++ app_lettings/tests.py
@@ -1,3 +1,40 @@
+# django imports
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+# app imports
+from .models import (
+    Address,
+    Letting
+)
+
+
+class LettingsPageTests(TestCase):
+    def setUp(self):
+        self.address = Address.objects.create(
+            number='1',
+            street='Test Street',
+            city='Test City',
+            state='Test State',
+            zip_code='12345',
+            country_iso_code='US',
+        )
+        self.letting = Letting.objects.create(
+            title='Test Letting',
+            address=self.address,
+        )
+
+    def test_lettings_index(self):
+        response = self.client.get(reverse('app_lettings:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_lettings/index.html')
+        self.assertIn(b'<title>Lettings</title>', response.content)
+
+    def test_lettings_detail(self):
+        response = self.client.get(reverse('app_lettings:letting', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'app_lettings/letting.html')
+
+    def test_lettings_models(self):
+        assert str(self.address) == f'{self.address.number} {self.address.street}'
+        assert str(self.letting) == f'{self.letting.title}'
