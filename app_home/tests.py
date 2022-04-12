910b279e229c9f3adcd593b0f7621cf117d96eff
AatroXissTV <antoine.beaudesson@gmail.com>
2022-04-12 11:32:34 +0200

    v.0.1.1 added tests for home page

--- app_home/tests.py
+++ app_home/tests.py
@@ -1,3 +1,11 @@
+# django imports
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTests(TestCase):
+    def test_index(self):
+        response = self.client.get(reverse('app_home:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+        self.assertIn(b'<title>Holiday Homes</title>', response.content)
