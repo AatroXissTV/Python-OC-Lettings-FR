477a808adc5eb9ddcd2dae969f3d82a9ddb68e57
AatroXissTV <antoine.beaudesson@gmail.com>
2022-04-18 09:46:22 +0200

    settings is now updated

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,15 +1,6 @@
 import os
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-from django.urls import path
-
-def trigger_error(request):
-    division_by_zero = 1 / 0
-
-urlpatterns = [
-    path('sentry-debug/', trigger_error),
-    # ...
-]
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
