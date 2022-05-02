dd6a71c86071fd06c1dce5b65a5ed7d21df86e40
AatroXissTV <antoine.beaudesson@gmail.com>
2022-05-02 14:59:14 +0200

    v.0.3.15 sentry modifications

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
+def trigger_error(request):
+    division_by_zero = 1 / 0  # noqa
+
+
 urlpatterns = [
+    path('sentry-debug/', trigger_error),
     path('', include('app_home.urls')),
     path('lettings/', include('app_lettings.urls')),
     path('profiles/', include('app_profiles.urls')),
